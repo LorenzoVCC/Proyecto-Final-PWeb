6333a15a57199109e2dbd95edda2e214be917b35
LorenzoVCC <Lorenzovecchia1@gmail.com>
2026-02-02 23:18:02 -0300

    Ruteo de clientes Corregido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,40 +1,51 @@
 import { Routes } from '@angular/router';
+
 import { LoginPage } from './pages/login-page/login-page';
 import { RegisterPage } from './pages/register-page/register-page';
+
 import { RestaurantListPage } from './pages/restaurant-list-page/restaurant-list-page';
 import { RestaurantPage } from './pages/restaurant-page/restaurant-page';
+
 import { ProductPage } from './pages/product-page/product-page';
+import { NewProductPage } from './pages/new-product-page/new-product-page';
+
 import { ClientLayout } from './layouts/client-layout/client-layout';
 import { NewCategoryPage } from './pages/new-category-page/new-category-page';
-import { NewProductPage } from './pages/new-product-page/new-product-page';
+
 import { authGuard } from './guards/auth-guard';
 import { ownerGuard } from './guards/owner-guard';
+import { publicGuard } from './guards/public-guard';
 
 export const routes: Routes = [
   {
+    path: "",
+    component: ClientLayout,
+    children: [
+      { path: "", component: RestaurantListPage },
+      { path: "restaurant-page/:id", component: RestaurantPage },
+      { path: "product-page/:id", component: ProductPage },
+    ],
+  },
+  {
     path: "login",
     component: LoginPage,
+    canActivate: [publicGuard]
   },
   {
     path: "register",
     component: RegisterPage,
+    canActivate: [publicGuard]
   },
+
   {
     path: "",
     component: ClientLayout,
     canActivateChild: [authGuard],
     children: [
       {
-        path: "",
-        component: RestaurantListPage,
-      },
-      {
-        path: 'restaurant-page/:id',
-        component: RestaurantPage,
-      },
-      {
         path: "edit-restaurant/:restaurantId",
-        component: RegisterPage
+        component: RegisterPage,
+        canActivate: [ownerGuard]
       },
       {
         path: "create-category/:restaurantId", 
@@ -58,6 +69,7 @@ export const routes: Routes = [
       {
         path: "product-page/:id",
         component: ProductPage,
+        canActivate: [ownerGuard],
       },
     ],
   },
--- src/app/guards/owner-guard.ts
+++ src/app/guards/owner-guard.ts
@@ -1,23 +1,25 @@
-import { inject, Inject } from '@angular/core';
-import { CanActivateFn, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, RedirectCommand, Router } from '@angular/router';
 import { Auth } from '../services/auth-service';
 //import { fakeAsync } from '@angular/core/testing';
 
-export const ownerGuard: CanActivateFn = (childroute, state) => {
+export const ownerGuard: CanActivateFn = (route, state) => {
   const auth = inject(Auth);
   const router = inject(Router);
-
-  const restaurantId = Number(childroute.paramMap.get('restaurantId'));
-
+  
   if (!auth.token) 
   {
     router.navigate(['/login']);
     return false;
   }
 
-  const myId = auth.restaurantId;
+  const restaurantId = Number(route.paramMap.get('restaurantId'));
+  if (Number.isNaN(restaurantId)) {
+    router.navigate(['/']);
+    return false;
+  }
 
-  if (myId === null || myId !== auth.restaurantId) {
+  if (auth.restaurantId === null || auth.restaurantId !== restaurantId) {
     router.navigate(['/']);
     return false;
   }
